680393f9b00b490f44157be06add9fef13b3f6b6
coppermouse <124282214+coppermouse@users.noreply.github.com>
2023-10-21 14:11:42 +0200

    set up base for tests

---
+++ source/tests/__init__.py
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# file: tests/__init__.py
+# ----------------------------------------
+
+
+import os
+import sys
+
+source_dir = os.path.normpath( os.path.join( os.path.dirname( __file__ ), '..', 'source' ) )
+sys.path.append( source_dir )
+
+
